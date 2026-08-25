f6d380dd286feecddd3eb777205557ec8991bf43
anshul <go4paris@gmail.com>
2026-08-25 14:26:52 +0530

    updated des size 3

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -30,8 +30,8 @@ module "eks" {
   kubernetes_version = "1.35"
   instance_types     = ["t3.small"]
   min_size           = 1
-  max_size           = 8
-  desired_size       = 6
+  max_size           = 5
+  desired_size       = 3
 }
 
 module "rds" {
